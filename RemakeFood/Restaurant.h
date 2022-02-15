1671b6fee455d41a378609f3acc202f0ed11c8f6
Artem <minnebaevartem@gmail.com>
2022-02-15 14:09:41 +0300

    добавил списки в ресторан

--- RemakeFood/Restaurant.h
+++ RemakeFood/Restaurant.h
@@ -1,12 +1,18 @@
 #pragma once
+#include "Order.h"
+#include "Dish.h"
 #include <string>
+#include <list>
 
 using namespace std;
 
 class Restaurant
 {
+public:
 
 private:
 	string Name;
 	string Adress;
+	list <Order> Orders;
+	list <Dish> Menu;
 };
