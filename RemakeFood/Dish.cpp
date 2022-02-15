60893ac65ba0fe8cb57de66c9d807db53684539a
Artem <minnebaevartem@gmail.com>
2022-02-15 13:24:13 +0300

    допилил геттеры и сеттеры блюду

--- RemakeFood/Dish.cpp
+++ RemakeFood/Dish.cpp
@@ -2,15 +2,37 @@
 
 string Dish::GetServingSize()
 {
+	return this->ServingSize;
+}
 
+Dish::Dish(string name, string size, double price)
+{
+	this->Name = name;
+	this->ServingSize = size;
+	this->PricePorcion = price;
 }
 
 string Dish::GetName()
 {
-
+	return this->Name;
 }
 
 double Dish::GetPricePorcion()
 {
-	return 0.0;
+	return this->PricePorcion;
+}
+
+void Dish::SetName(string name)
+{
+	this->Name = name;
+}
+
+void Dish::SetPricePorcion(double price)
+{
+	this->PricePorcion = price;
+}
+
+void Dish::SetServingSize(string size)
+{
+	this->ServingSize = size;
 }
--- RemakeFood/Dish.h
+++ RemakeFood/Dish.h
@@ -6,9 +6,14 @@ using namespace std;
 class Dish
 {
 public:
+	Dish(string name, string size, double price);
 	string GetName();
 	string GetServingSize();
 	double GetPricePorcion();
+	void SetName(string name);
+	void SetPricePorcion(double price);
+	void SetServingSize(string size);
+
 
 private:
 	string Name;
